88eef07296ba7388e51ff5facc2cbb89f3a72d7e
achess <chout1213@gmail.com>
2022-11-05 16:32:27 +0900

    durability image part

--- src/components/DurabilityImage.js
+++ src/components/DurabilityImage.js
@@ -88,6 +88,34 @@ const DurabilityImage = ({ screenX }) => {
                     }px)`;
                 }
             }
+        } else if (screenX >= 768 && screenX < 1024) {
+            if (scroll >= 5700 && scroll < 8100) {
+                if (image1) {
+                    image1.style.transform = `translate(32vw, ${
+                        230 - (scroll - 5700) * 0.05
+                    }px)`;
+                }
+                if (image2) {
+                    image2.style.transform = `translate(58vw, ${
+                        -190 - (scroll - 5700) * 0.05
+                    }px)`;
+                }
+                if (image3) {
+                    image3.style.transform = `translate(12vw, ${
+                        -1 * (scroll - 5700) * 0.015
+                    }px)`;
+                }
+                if (image4) {
+                    image4.style.transform = `translate(30vw, ${
+                        320 - (scroll - 5700) * 0.015
+                    }px)`;
+                }
+                if (imageBackground) {
+                    imageBackground.style.transform = `translate(49vw, ${
+                        -310 - (scroll - 5700) * 0.05
+                    }px)`;
+                }
+            }
         } else if (screenX < 768) {
             if (scroll >= 5000 && scroll < 9200) {
                 if (image1) {
