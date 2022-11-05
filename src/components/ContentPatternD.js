e02ab10157ea202ec559ed36089e70a7b404c7f3
achess <chout1213@gmail.com>
2022-11-05 16:34:43 +0900

    content pattern D

--- src/components/ContentPatternD.js
+++ src/components/ContentPatternD.js
@@ -62,6 +62,36 @@ const ContentPatternD = ({
                     setGlassState(false);
                 }
             }
+        } else if (screenX >= 768 && screenX < 1024) {
+            if (scroll > 8200 && scroll < 9100) {
+                if (upperGlass && lowerGlass && !glassState) {
+                    setGlassState(true);
+                    upperGlass.style.transition = `0.7s all ease-in-out`;
+                    lowerGlass.style.transition = `0.7s all ease-in-out`;
+                    setTimeout(() => {
+                        upperGlass.style.transform = `translate(0px, -20px)`;
+                        lowerGlass.style.transform = `translate(0px, -20px)`;
+                    }, 200);
+                    setTimeout(() => {
+                        upperGlass.style.transform = `translate(0px, 0px)`;
+                        lowerGlass.style.transform = `translate(0px, 0px)`;
+                    }, 1000);
+                    setTimeout(() => {
+                        upperGlass.style.opacity = 0;
+                        lowerGlass.style.opacity = 0;
+                    }, 1000);
+                }
+            } else {
+                if (upperGlass && lowerGlass) {
+                    upperGlass.style.opacity = 1;
+                    lowerGlass.style.opacity = 1;
+                    upperGlass.style.transition = `none`;
+                    lowerGlass.style.transition = `none`;
+                    upperGlass.style.transform = `translate(0px, -60px)`;
+                    lowerGlass.style.transform = `translate(0px, -30px)`;
+                    setGlassState(false);
+                }
+            }
         } else if (screenX < 768) {
             if (scroll > 7900 && scroll < 8500) {
                 if (upperGlass && lowerGlass && !glassState) {
